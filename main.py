9b33f546bfb249a48f74700f9d865fa7d6cff6d9
tristan kalos <tristan.kalos@live.fr>
2019-06-13 19:32:09 +0200

    Improve refac.
    Signed-off-by: tristan kalos <tristan.kalos@live.fr>

--- main.py
+++ main.py
@@ -3,40 +3,32 @@
 #!/bin/bash
 # usage ./docker.sh image-name
 # retrieves node libs, python libs and disk usage information for a dockerhub image
-
 import sys
 import json
+
+from utils import mount_docker_image
 
 from analyzers.base import get_base_info
 from analyzers.native_packages import get_native_packages_info
 from analyzers.python_packages import get_python_packages_info
 from analyzers.node_packages import get_node_packages_info
 
+
 def get_image_info(image):
-    # Running the container
-    container = get_ipython().getoutput(f'sudo docker create {image}').pop()
 
-    # Copying its content in a temporary directory
-    tmp_dir = get_ipython().getoutput(f'mktemp -d -t dc-XXXXXXXXXX').pop()
+    with mount_docker_image(image) as image_path:
 
-    get_ipython().system(f'docker export  {container} > temp.tar')
-    get_ipython().system(f'docker rm -f {container} > /dev/null')
-    get_ipython().system(f'tar fx temp.tar -C {tmp_dir}')
+        distro, version, image_size = get_base_info(image_path)
+        
+        natives_packages_info = get_native_packages_info(image_path, distro)
 
-    distro, version, image_size = get_base_info(tmp_dir)
+        python_packages_info = get_python_packages_info(image_path)
+        
+        node_packages_info = get_node_packages_info(image_path)
     
-    natives_packages_info = get_native_packages_info(tmp_dir, distro)
-
-    python_packages_info = get_python_packages_info(tmp_dir)
-    
-    node_packages_info = get_node_packages_info(tmp_dir)
-
-    # cleaning up
-    get_ipython().system('sudo rm -rf temp.tar')
-    get_ipython().system(f'sudo rm -rf {tmp_dir}')
 
     return json.dumps({
-            'slug': image,
+            'image': image,
             'size': image_size,
             'distribution': distro,
             'version': version,
@@ -47,5 +39,5 @@ def get_image_info(image):
             }
         }, indent=4)
 
-
-print(get_image_info('faizanbashir/python-datascience'))
+if __name__ == "__main__":
+    print(get_image_info('amancevice/superset'))
