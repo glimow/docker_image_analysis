e5b8fd7dfd68ea8c57a9cb419ed3430c8b39f608
tristan kalos <tristan.kalos@live.fr>
2019-07-03 17:48:09 +0200

    Fix python dependency not being fetched correctly.
    Signed-off-by: tristan kalos <tristan.kalos@live.fr>

--- analyzer/analyzers/python_packages.py
+++ analyzer/analyzers/python_packages.py
@@ -4,32 +4,51 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+def process_one_package(path, package, python_version="3"):
+    command = f"sudo chroot {path} pip{python_version} show {package}"
+    info = get_ipython().getoutput(command)
+    for line in info:
+        if "Name" in line:
+            name = line.split(" ").pop()
+        if "Version" in line:
+            version = line.split(" ").pop()
+        if "Location" in line:
+            location = line.split(" ").pop()
+    result = get_ipython().getoutput(
+        f"du --max-depth=0 {path}{location}/{name}").pop()
+    # If the folder does not exist, try lowercase
+    if "cannot access" in result:
+        result = get_ipython().getoutput(
+            f"du --max-depth=0 {path}{location}/{name.lower()}").pop()
+    # If the lowercase folder do not exist either
+    if "cannot access" not in result:
+        size = int(result.split('\t').pop(0))
+    # List the files by hand
+    else:
+        command = f"sudo chroot {path} pip{python_version} show {package} -f"
+        info = get_ipython().getoutput(command)
+        flag = False
+        size = 0
+        for line in info:
+            if flag:
+                command = f"du {path}{location}/{line.strip()}"
+                size += int(get_ipython().getoutput(command).pop().split('\t').pop(0))
+            if 'Files' in line:
+                flag = True
+    return [name, version, size]
+
 def get_python_packages_info(path, python_version="3"):
 
     command = f"sudo chroot {path} pip{python_version} list --format freeze --no-cache-dir 2>/dev/null"
 
     packages = [package.split('==')
                 for package in get_ipython().getoutput(command)]
-    
-    command = """sudo chroot """ + path + \
-        """ pip"""+ python_version + \
-        """ list --format freeze --no-cache-dir | awk -F = {'print $1'}|""" \
-        """ xargs pip"""+ python_version +""" show | grep -E 'Location:|Name:' | cut -d ' ' -f 2 |"""\
-        """ paste -d ' ' - - | awk '{print $2 "/" tolower($1)}' | xargs du -s 2> /dev/null"""
-
-    sizes_raw = get_ipython().getoutput(command)
 
     package_list = []
-    for raw_line in sizes_raw:
+    for package in packages:
         try:
-            line = raw_line.split("\t")
-            size = line.pop(0)
-            package = line.pop().split("/").pop()
-            for package_version in packages:
-                if package in package_version[0]:
-                    version = package_version[1]
-                    package_list.append([package, version, int(size)])
+            package_list.append(process_one_package(path, package[0]))
         except Exception as e:
-            logger.error("Error processing python packages", e)
+            logger.error("Error processing python packages", package[0], e)
             pass
     return package_list
